cb5270b2c443d5cf25eaa94d4f02e22a822c2a5f
Hakim Nizami <hakimnizami15@gmail.com>
2025-11-11 00:54:05 +0700

    fix: gives session lifetime (i'm so dum asf)

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -17,6 +17,9 @@ export const auth = betterAuth({
     enabled: true,
   },
   fetchOptions: { credentials: 'include' },
+  session: {
+    expiresIn: 60 * 60 * 24 * 30, // 30 days
+  },
   socialProviders: {
     google: {
       prompt: 'select_account',
